2127552ddcd4af6c20abaa86c9f6508728ae7dc1
Sofyan <sofyan.saputra@ralali.com>
2020-01-01 22:01:56 +0700

    Add: Client Entity

--- entity/aws.go
+++ entity/aws.go
@@ -2,11 +2,9 @@ package entity
 
 // AwsConfig ...
 type AwsConfig struct {
-	PathURL            string
 	AwsAccessKeyID     string
 	AwsSecretAccessKey string
 	APArea             string
-	Broker             string
 	ShardID            string
 	StreamName         string
 }
